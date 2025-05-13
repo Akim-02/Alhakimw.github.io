a48bf909bbc5d5ad2a1290bdb4f71ac7557cdf00
alhakimwattimena <alhakimwatimena@gmail.com>
2025-05-13 20:02:14 +0700

    belajar js

--- js/server.js
+++ js/server.js
@@ -4,3 +4,14 @@ const menu = document.querySelector('.ul-navbar');
 burger.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+let scedule={
+ Hari:['senin','selasa','rabu','kamis','jum\'at','sabtu','minggu'],
+ kegiatan:['sekolah','sekolah','sekolah','sekolah','sekolah','sekolah','libur'],
+ play: function() {
+  for (let i = 0; i < this.Hari.length; i++) {
+    console.log(`hari ${this.Hari[i]} gw ${this.kegiatan[i]}`)
+  }
+ }
+}
+scedule.play();
